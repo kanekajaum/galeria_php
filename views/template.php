aca93a3e68f57d12946197a776fdf222e3d6a72d
Joao <joaosantos.rodrigues9@gmail.com>
2019-04-22 14:01:35 -0400

    first commit

---
+++ views/template.php
@@ -0,0 +1,13 @@
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Galeria de Fotos</title>
+	</head>
+	<body>
+		<h1>Topo</h1>
+		
+		<?php
+		 	$this->loadViewInTemplate($viewName, $viewData); 
+		 ?>
+	</body>
+</html>
