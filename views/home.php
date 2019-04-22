69b2bf2fd2a26730a10c8f5482a25581fe78a9a2
Joao <joaosantos.rodrigues9@gmail.com>
2019-04-22 14:30:34 -0400

    fotos add

--- views/home.php
+++ views/home.php
@@ -1,7 +1,7 @@
 <?php  
 foreach ($fotos as $foto): ?>
 
-<img src="assets/images/galeria/<?php echo $foto['url']; ?>" width="300" border="0" wid><br>
+<img src="assets/images/galeria/<?php echo $foto['url']; ?>" width="300" border="0" style="margin:50;"><br>
 <?php echo $foto['titulo']; ?>
 <hr>
 
